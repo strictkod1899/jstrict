e86914aa4b13333eab4a820ed8b1cbeccad62dd0
Konstantin <strictkod1899@yandex.ru>
2018-04-21 15:26:46 +0700

    refactor (jdb-spring): override a methods 'read' and 'readAll'

--- jdb/jdb-spring/src/main/java/ru/strict/db/repositories/StrictRepositorySpringBase.java
+++ jdb/jdb-spring/src/main/java/ru/strict/db/repositories/StrictRepositorySpringBase.java
@@ -1,10 +1,17 @@
 package ru.strict.db.repositories;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import ru.strict.db.connections.StrictCreateConnectionByDataSource;
 import ru.strict.db.dto.StrictDtoBase;
 import ru.strict.db.entities.StrictEntityBase;
 import ru.strict.db.mappers.StrictMapperBase;
+import ru.strict.db.requests.StrictDbRequests;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Базовый класс репозитория с использованием Spring Jdbc
@@ -23,13 +30,38 @@ public abstract class StrictRepositorySpringBase
      */
     private NamedParameterJdbcTemplate springJdbc;
 
+    /**
+     * Объект для преобразования полученных данных из sql-запроса в объект сущности азы данных (entity)
+     */
+    private RowMapper<E> springMapper;
+
     //<editor-fold defaultState="collapsed" desc="constructors">
-    public StrictRepositorySpringBase(SOURCE connectionSource, StrictMapperBase mapper, String sqlSelect) {
+    public StrictRepositorySpringBase(SOURCE connectionSource, StrictMapperBase mapper, String sqlSelect,
+                                      RowMapper<E> springMapper) {
         super(connectionSource, mapper);
         this.sqlSelect = sqlSelect;
         this.springJdbc = new NamedParameterJdbcTemplate(getConnectionSource().getConnectionSource());
+        this.springMapper = springMapper;
     }
     //</editor-fold>
+
+    @Override
+    public DTO read(ID id) {
+        String sql = String.format("%s WHERE id = :entityId", getSqlSelect());
+        SqlParameterSource parameters = new MapSqlParameterSource("entityId", id);
+        E entity = springJdbc.queryForObject(sql, parameters, springMapper);
+        return getMapper().map(entity);
+    }
+
+    @Override
+    public List<DTO> readAll(StrictDbRequests requests) {
+        String sql = getSqlSelect() + (requests==null?"":requests.getSql());
+        List<DTO> result = new LinkedList<>();
+        List<E> entities = springJdbc.query(sql, springMapper);
+        for(E entity : entities)
+            result.add(getMapper().map(entity));
+        return result;
+    }
 
     //<editor-fold defaultState="collapsed" desc="Get/Set">
     public String getSqlSelect() {
