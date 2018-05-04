28481afc1ce6229918796fbe85db80ad347a540f
Konstantin <konstantin.kastirin@actonica.ru>
2018-05-04 12:41:01 +0800

    feature (logger): added a create StrictLogger to 'jutils'

--- jutils/src/main/java/ru/strict/utils/components/StrictLogger.java
+++ jutils/src/main/java/ru/strict/utils/components/StrictLogger.java
@@ -1,56 +1,47 @@
-package ru.strict.utils;
+package ru.strict.utils.components;
 
 import org.apache.log4j.Logger;
-import ru.strict.utils.components.StrictLogger;
-
-import static org.apache.log4j.Logger.getLogger;
 
 /**
- * Управление логированием
+ * Обертка для логгера
  */
-public class StrictUtilLogger {
+public class StrictLogger {
 
-    public static StrictLogger createLogger(Class clazz){
-        return new StrictLogger(getLogger(clazz));
+    private final Logger logger;
+
+    public StrictLogger(Logger logger) {
+        this.logger = logger;
     }
 
     /**
      * Информационное сообщение логирования для Debug-режима
-     * @param clazz Класс, в котором производится логирование
      * @param message Сообщение исключения
      */
-    public static void trace(Class clazz, String message){
-        Logger logger = getLogger(clazz);
+    public void trace(String message){
         logger.trace(message);
     }
 
     /**
      * Информационное сообщение логирования
-     * @param clazz Класс, в котором производится логирование
      * @param message Сообщение исключения
      */
-    public static void info(Class clazz, String message){
-        Logger logger = getLogger(clazz);
+    public void info(String message){
         logger.info(message);
     }
 
     /**
      * Предупреждающее сообщения логирования
-     * @param clazz Класс, в котором производится логирование
      * @param message Сообщение исключения
      */
-    public static void warn(Class clazz, String message){
-        Logger logger = getLogger(clazz);
+    public void warn(String message){
         logger.warn(message);
     }
 
     /**
      * Логирование исключения
-     * @param clazz Класс, в котором производится логирование
      * @param message Сообщение исключения
      */
-    public static void error(Class clazz, String message){
-        Logger logger = getLogger(clazz);
+    public void error(String message){
         logger.error(message);
     }
 
@@ -60,12 +51,10 @@ public class StrictUtilLogger {
      * <code><pre style="background-color: white; font-family: consolas">
      * StrictUtilLogger.error(MyClass.class, ex.getClass().toString(), ex.getMessage());
      * </pre></code>
-     * @param clazz Класс, в котором производится логирование
      * @param type Тип исключения
      * @param message Сообщение исключения
      */
-    public static void error(Class clazz, String type,  String message){
-        Logger logger = getLogger(clazz);
+    public void error(String type,  String message){
         logger.error(String.format("%s - %s", type, message));
     }
 
@@ -75,13 +64,33 @@ public class StrictUtilLogger {
      * <code><pre style="background-color: white; font-family: consolas">
      * StrictUtilLogger.error(MyClass.class, "My message", ex.getClass().toString(), ex.getMessage());
      * </pre></code>
-     * @param clazz Класс, в котором производится логирование
      * @param customMessage Пользовательское (дополнительное) сообщение
      * @param type Тип исключения
      * @param message Сообщение исключения
      */
-    public static void error(Class clazz, String customMessage, String type,  String message){
-        Logger logger = getLogger(clazz);
+    public void error(String customMessage, String type,  String message){
         logger.error(String.format("%s \n %s - %s", customMessage, type, message));
     }
+
+    //<editor-fold defaultState="collapsed" desc="Get/Set">
+    public Logger getLogger() {
+        return logger;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultState="collapsed" desc="Base override">
+    @Override
+    public String toString(){
+        return String.format("Logger: %s", logger.getName());
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof StrictLogger) {
+            StrictLogger object = (StrictLogger) obj;
+            return logger.equals(object.getLogger());
+        }else
+            return false;
+    }
+    //</editor-fold>
 }
