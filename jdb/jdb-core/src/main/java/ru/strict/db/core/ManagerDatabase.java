b1691b3559f150f4147c52089713f63d9afbc750
Konstantin <strictkod1899@yandex.ru>
2018-06-24 09:33:20 +0700

    refactor (jdb-core): added a methods to ManagerDatabase

--- jdb/jdb-core/src/main/java/ru/strict/db/core/ManagerDatabase.java
+++ jdb/jdb-core/src/main/java/ru/strict/db/core/ManagerDatabase.java
@@ -1,17 +1,22 @@
 package ru.strict.db.core;
 
+import ru.strict.db.core.connections.CreateConnectionFactory;
 import ru.strict.db.core.connections.ICreateConnection;
 import ru.strict.db.core.migration.MigrationDatabase;
 import ru.strict.db.core.migration.components.MigrationTable;
 import ru.strict.db.core.repositories.IRepository;
+import ru.strict.utils.UtilReflection;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.sql.Connection;
 
 /**
  * Управление соединением с базой данных
  */
-public class ManagerDatabase<SOURCE extends ICreateConnection> {
+public class ManagerDatabase<SOURCE> {
 
     /**
      * Источник подключения к базе данных (используется для получения объекта Connection),
@@ -33,9 +38,33 @@ public class ManagerDatabase<SOURCE extends ICreateConnection> {
     public ManagerDatabase(SOURCE connectionSource) {
         this.connectionSource = connectionSource;
         repositories = new LinkedHashMap<>();
-        migration = new MigrationDatabase(connectionSource);
+        migration = new MigrationDatabase(new CreateConnectionFactory().instance(getConnectionSource()));
     }
     //</editor-fold>
+
+    public <REPOSITORY extends IRepository> IRepository createRepository(Class<REPOSITORY> repositoryClass,
+                                                                         Object...parameters){
+        Object[] userParameters = new Object[parameters.length + 1];
+        userParameters[0] = getCreateConnection();
+        for(int i=0; i<parameters.length; i++){
+            userParameters[i+1] = parameters[i];
+        }
+
+        return UtilReflection.createInstance(repositoryClass, userParameters);
+    }
+
+    /**
+     * Добавить новый репозиторий
+     * @param key Ключ доступа к репозиторию
+     */
+    public <REPOSITORY extends IRepository> IRepository createAndAddRepository(
+            String key,
+            Class<REPOSITORY> repositoryClass,
+            Object...parameters) {
+        IRepository repository = createRepository(repositoryClass, parameters);
+        repositories.put(key, repository);
+        return repository;
+    }
 
     public void migration(){
         migration.migration();
@@ -69,6 +98,33 @@ public class ManagerDatabase<SOURCE extends ICreateConnection> {
     }
 
     /**
+     * Получить репоизтории по классу
+     * @return
+     */
+    public <REPSITORY extends IRepository> IRepository[] getRepositories(Class<REPSITORY> repositoryClass) {
+        IRepository[] result = repositories.entrySet()
+                .stream()
+                .filter(entry -> UtilReflection.IsInstanceOf(repositoryClass, entry.getValue().getClass()))
+                .map(entry -> entry.getValue())
+                .toArray(IRepository[]::new);
+        return result;
+    }
+
+    /**
+     * Получить репоизтории по классу
+     * @return
+     */
+    public <REPSITORY extends IRepository> IRepository getRepository(Class<REPSITORY> repositoryClass) {
+        IRepository result = repositories.entrySet()
+                .stream()
+                .filter(entry -> UtilReflection.IsInstanceOf(repositoryClass, entry.getValue().getClass()))
+                .map(entry -> entry.getValue())
+                .findFirst()
+                .orElse(null);
+        return result;
+    }
+
+    /**
      * Получить репоизторий по ключу
      * @param key Ключ доступа к репозиторию
      * @return
@@ -79,6 +135,16 @@ public class ManagerDatabase<SOURCE extends ICreateConnection> {
 
     public SOURCE getConnectionSource() {
         return connectionSource;
+    }
+
+    public ICreateConnection getCreateConnection(){
+        return new CreateConnectionFactory().instance(getConnectionSource());
+    }
+
+    public Connection getConnection(){
+        return new CreateConnectionFactory()
+                .instance(getConnectionSource())
+                .createConnection();
     }
     //</editor-fold>
 }
