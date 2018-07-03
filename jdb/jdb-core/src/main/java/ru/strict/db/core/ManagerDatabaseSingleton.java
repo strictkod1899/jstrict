fd1040c4fc1b8fc85658c13c5f95add2320f8a8b
Konstantin <konstantin.kastirin@actonica.ru>
2018-07-03 11:19:06 +0800

    feature (jdb-core): implemented ManagerDatabase as Singleton

---
+++ jdb/jdb-core/src/main/java/ru/strict/db/core/ManagerDatabaseSingleton.java
@@ -0,0 +1,20 @@
+package ru.strict.db.core;
+
+public class ManagerDatabaseSingleton<SOURCE> extends ManagerDatabase<SOURCE> {
+
+    private static ManagerDatabase instance;
+
+    private ManagerDatabaseSingleton(SOURCE connectionSource) {
+        super(connectionSource);
+    }
+
+    public void initialize(SOURCE connectionSource){
+        if(instance == null){
+            instance = new ManagerDatabaseSingleton(connectionSource);
+        }
+    }
+
+    public static ManagerDatabase instance(){
+        return instance;
+    }
+}
