164381d118f3e1a30143e0c4eceb0a682c6a420d
Konstantin <konstantin.kastirin@actonica.ru>
2018-07-24 12:57:47 +0800

    refactor (jdb-jdbc): added a close connection after query

--- jdb/jdb-jdbc/src/main/java/ru/strict/db/jdbc/repositories/RepositoryJdbcBase.java
+++ jdb/jdb-jdbc/src/main/java/ru/strict/db/jdbc/repositories/RepositoryJdbcBase.java
@@ -56,8 +56,8 @@ public abstract class RepositoryJdbcBase
                 parameters = getParameters(entity, 0);
                 sql = createSqlInsertShort(parameters.size());
 
-                try {
-                    statement = createConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                try (Connection connection = createConnection()){
+                    statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
                     statement = setParametersToPrepareStatement(statement, parameters);
                     statement.executeUpdate();
 
@@ -77,8 +77,8 @@ public abstract class RepositoryJdbcBase
                 parameters.add(0, "id", id);
                 sql = createSqlInsertFull(parameters.size()-1);
 
-                try {
-                    statement = createConnection().prepareStatement(sql);
+                try (Connection connection = createConnection()){
+                    statement = connection.prepareStatement(sql);
                     statement = setParametersToPrepareStatement(statement, parameters);
                     statement.executeUpdate();
                 } catch (SQLException e) {
@@ -91,8 +91,8 @@ public abstract class RepositoryJdbcBase
                 parameters = getParameters(entity, 1);
                 parameters.add(0, "id", dto.getId());
                 sql = createSqlInsertFull(parameters.size()-1);
-                try {
-                    statement = createConnection().prepareStatement(sql);
+                try (Connection connection = createConnection()){
+                    statement = connection.prepareStatement(sql);
                     statement = setParametersToPrepareStatement(statement, parameters);
                     statement.executeUpdate();
                 } catch (SQLException e) {
@@ -113,8 +113,8 @@ public abstract class RepositoryJdbcBase
     public DTO read(ID id) {
         PreparedStatement statement;
         ResultSet resultSet;
-        try {
-            statement = createConnection().prepareStatement(String.format("%s WHERE id = ?", createSqlSelect()));
+        try (Connection connection = createConnection()){
+            statement = connection.prepareStatement(String.format("%s WHERE id = ?", createSqlSelect()));
 
             if(id instanceof Integer)
                 statement.setInt(1, (Integer) id);
@@ -142,9 +142,9 @@ public abstract class RepositoryJdbcBase
         PreparedStatement statement;
         ResultSet resultSet;
         List<DTO> result = new LinkedList<>();
-        try {
+        try (Connection connection = createConnection()){
             String sql = createSqlSelect() + (requests==null ? "" : requests.getSql());
-            statement = createConnection().prepareStatement(sql);
+            statement = connection.prepareStatement(sql);
             resultSet = statement.executeQuery();
             while(resultSet.next())
                 result.add(getDtoMapper().map(sqlMapper.map(resultSet)));
@@ -167,8 +167,8 @@ public abstract class RepositoryJdbcBase
 
         PreparedStatement statement;
 
-        try {
-            statement = createConnection().prepareStatement(sql);
+        try (Connection connection = createConnection()){
+            statement = connection.prepareStatement(sql);
             statement = setParametersToPrepareStatement(statement, parameters);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -186,8 +186,8 @@ public abstract class RepositoryJdbcBase
         parameters.addLast("id", id);
         PreparedStatement statement;
 
-        try {
-            statement = createConnection().prepareStatement(sql);
+        try (Connection connection = createConnection()){
+            statement = connection.prepareStatement(sql);
             statement = setParametersToPrepareStatement(statement, parameters);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -287,8 +287,8 @@ public abstract class RepositoryJdbcBase
         parameters.addLast("id", id);
         PreparedStatement statement;
         boolean isExists = false;
-        try {
-            statement = createConnection().prepareStatement(sql);
+        try (Connection connection = createConnection()){
+            statement = connection.prepareStatement(sql);
             statement = setParametersToPrepareStatement(statement, parameters);
 
             ResultSet resultSet = statement.executeQuery();
