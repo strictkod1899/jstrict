f704585077208cc11ab8202e6b1c0d207a9b5897
Konstantin <strictkod1899@yandex.ru>
2018-09-02 20:29:09 +0700

    feature (jdb-spring): implemented an interfaces for repository user and jwt token

--- jdb/jdb-spring/src/main/java/ru/strict/db/spring/repositories/RepositoryUser.java
+++ jdb/jdb-spring/src/main/java/ru/strict/db/spring/repositories/RepositoryUser.java
@@ -10,6 +10,7 @@ import ru.strict.db.core.entities.EntityUser;
 import ru.strict.db.core.entities.EntityUserOnRole;
 import ru.strict.db.core.repositories.IRepository;
 import ru.strict.db.core.repositories.RepositoryBase;
+import ru.strict.db.core.repositories.interfaces.IRepositoryUser;
 import ru.strict.db.core.requests.DbRequests;
 import ru.strict.db.core.requests.DbWhere;
 import ru.strict.db.core.mappers.dto.MapperDtoBase;
@@ -19,9 +20,11 @@ import ru.strict.utils.UtilLogger;
 import java.util.*;
 
 public class RepositoryUser<ID, DTO extends DtoUserBase>
-        extends RepositoryNamedBase<ID, EntityUser, DTO> {
+        extends RepositoryNamedBase<ID, EntityUser, DTO>
+        implements IRepositoryUser<ID, DTO> {
 
-    private static final String[] COLUMNS_NAME = new String[] {"username", "passwordencode"};
+    private static final String[] COLUMNS_NAME = new String[] {"username", "passwordencode", "email",
+            "isBlocked", "isDeleted", "isConfirmEmail"};
 
     public RepositoryUser(CreateConnectionByDataSource connectionSource,
                           MapperDtoBase<EntityUser, DTO> dtoMapper,
@@ -34,6 +37,10 @@ public class RepositoryUser<ID, DTO extends DtoUserBase>
         Map<Integer, Object> valuesByColumn = new LinkedHashMap();
         valuesByColumn.put(0, entity.getUsername());
         valuesByColumn.put(1, entity.getPasswordEncode());
+        valuesByColumn.put(2, entity.getEmail());
+        valuesByColumn.put(3, entity.isBlocked());
+        valuesByColumn.put(4, entity.isDeleted());
+        valuesByColumn.put(5, entity.isConfirmEmail());
         return valuesByColumn;
     }
 
@@ -60,6 +67,15 @@ public class RepositoryUser<ID, DTO extends DtoUserBase>
         requests.add(new DbWhere(repositoryProfile.getTableName(), "userx_id", dto.getId(), "="));
         dto.setProfile(repositoryProfile.readAll(requests).stream().findFirst().orElse(null));
         return dto;
+    }
+
+    @Override
+    public DTO readByEmail(String email) {
+        DbRequests requests = new DbRequests(getTableName(), true);
+        requests.add(new DbWhere(getTableName(), "email", email, "="));
+
+        DTO result = readAll(requests).stream().findFirst().orElse(null);
+        return result;
     }
 
     @Override
--- jdb/jdb-spring/src/main/java/ru/strict/db/spring/security/MapperDtoUserSecurity.java
+++ jdb/jdb-spring/src/main/java/ru/strict/db/spring/security/MapperDtoUserSecurity.java
@@ -1,27 +1,45 @@
 package ru.strict.db.spring.security;
 
+import ru.strict.db.core.dto.DtoProfile;
 import ru.strict.db.core.dto.DtoRoleuser;
+import ru.strict.db.core.entities.EntityProfile;
 import ru.strict.db.core.entities.EntityRoleuser;
 import ru.strict.db.core.mappers.dto.MapperDtoBase;
 import ru.strict.db.core.mappers.dto.MapperDtoRoleuser;
+
+import java.util.Optional;
 
 /**
  * Двухсторонний маппинг объектов типа EntityUserSecurity и DtoUserSecurity
  */
 public class MapperDtoUserSecurity extends MapperDtoBase<EntityUserSecurity, DtoUserSecurity> {
 
-    private MapperDtoRoleuser mapperRoleuser;
+    private MapperDtoBase<EntityRoleuser, DtoRoleuser> mapperRoleuser;
+    private MapperDtoBase<EntityProfile, DtoProfile> mapperProfile;
 
-    public MapperDtoUserSecurity(MapperDtoRoleuser mapperRoleuser){
+    public MapperDtoUserSecurity(){
+        this.mapperRoleuser = null;
+        this.mapperProfile = null;
+    }
+
+    public MapperDtoUserSecurity(MapperDtoBase<EntityRoleuser, DtoRoleuser> mapperRoleuser
+            , MapperDtoBase<EntityProfile, DtoProfile> mapperProfile){
         this.mapperRoleuser = mapperRoleuser;
+        this.mapperProfile = mapperProfile;
     }
 
     @Override
     protected EntityUserSecurity implementMap(DtoUserSecurity dto) {
         EntityUserSecurity entity = new EntityUserSecurity();
         entity.setId(dto.getId());
+        entity.setEmail(dto.getEmail());
+        entity.setBlocked(dto.isBlocked());
+        entity.setDeleted(dto.isDeleted());
+        entity.setConfirmEmail(dto.isConfirmEmail());
         entity.setUsername(dto.getUsername());
-        dto.getRolesuser().stream().forEach(r -> entity.addRoleuser(mapperRoleuser.map((DtoRoleuser) r)));
+        Optional.ofNullable(mapperRoleuser).ifPresent((mapper) ->
+                dto.getRolesuser().stream().forEach(r -> entity.addRoleuser(mapper.map((DtoRoleuser) r))));
+        Optional.ofNullable(mapperProfile).ifPresent((mapper) -> entity.setProfile(mapper.map(dto.getProfile())));
         entity.setPasswordEncode(dto.getPasswordEncode());
         return entity;
     }
@@ -31,7 +49,13 @@ public class MapperDtoUserSecurity extends MapperDtoBase<EntityUserSecurity, Dto
         DtoUserSecurity dto = new DtoUserSecurity();
         dto.setId(entity.getId());
         dto.setUsername(entity.getUsername());
-        entity.getRolesuser().stream().forEach(r -> dto.addRoleuser(mapperRoleuser.map((EntityRoleuser) r)));
+        dto.setEmail(entity.getEmail());
+        dto.setBlocked(entity.isBlocked());
+        dto.setDeleted(entity.isDeleted());
+        dto.setConfirmEmail(entity.isConfirmEmail());
+        Optional.ofNullable(mapperRoleuser).ifPresent((mapper) ->
+                entity.getRolesuser().stream().forEach(r -> dto.addRoleuser(mapper.map((EntityRoleuser) r))));
+        Optional.ofNullable(mapperProfile).ifPresent((mapper) -> dto.setProfile(mapper.map(entity.getProfile())));
         dto.setPasswordEncode(entity.getPasswordEncode());
         return dto;
     }
