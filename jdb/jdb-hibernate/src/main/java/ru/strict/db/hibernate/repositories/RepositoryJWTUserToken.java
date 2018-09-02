c36dd68b32c23de42b2f30afae8f3d6db6014689
Konstantin <strictkod1899@yandex.ru>
2018-09-02 14:34:23 +0700

    featue (jdb-hibernate): implemented an interfaces for repository user and jwt token

--- jdb/jdb-hibernate/src/main/java/ru/strict/db/hibernate/repositories/RepositoryJWTUserToken.java
+++ jdb/jdb-hibernate/src/main/java/ru/strict/db/hibernate/repositories/RepositoryJWTUserToken.java
@@ -1,13 +1,24 @@
 package ru.strict.db.hibernate.repositories;
 
+import org.hibernate.Session;
 import ru.strict.db.core.common.GenerateIdType;
 import ru.strict.db.core.common.MapperDtoType;
 import ru.strict.db.core.dto.DtoJWTUserToken;
+import ru.strict.db.core.repositories.interfaces.IRepositoryJWTToken;
 import ru.strict.db.hibernate.connection.CreateConnectionHibernate;
 import ru.strict.db.hibernate.entities.EntityJWTUserToken;
 import ru.strict.db.hibernate.mappers.dto.MapperDtoFactory;
 
-public class RepositoryJWTUserToken extends RepositoryHibernateBase<EntityJWTUserToken, DtoJWTUserToken> {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.UUID;
+
+public class RepositoryJWTUserToken
+        extends RepositoryHibernateBase<EntityJWTUserToken, DtoJWTUserToken>
+        implements IRepositoryJWTToken<UUID> {
 
     private static final String[] COLUMNS_NAME = new String[] {"accessToken", "refreshToken", "expireTimeAccess", "expireTimeRefresh",
             "issuedAt", "issuer", "subject", "notBefore", "audience", "secret", "algorithm", "type", "userx_id", "roleuser_id"};
@@ -33,5 +44,51 @@ public class RepositoryJWTUserToken extends RepositoryHibernateBase<EntityJWTUse
     @Override
     protected Class getThisClass() {
         return this.getClass();
+    }
+
+    @Override
+    public DtoJWTUserToken readByAccessToken(String caption) {
+        DtoJWTUserToken result = null;
+        try(Session session = createConnection()){
+            session.beginTransaction();
+            EntityManagerFactory entityManagerFactory = session.getEntityManagerFactory();
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<EntityJWTUserToken> criteriaEntity = criteriaBuilder.createQuery(getEntityClass());
+            Root<EntityJWTUserToken> criteriaRoot = criteriaEntity.from(getEntityClass());
+            criteriaEntity.select(criteriaRoot);
+            criteriaEntity.where(criteriaBuilder.equal(criteriaRoot.get("accessToken"), caption));
+            EntityJWTUserToken entity = entityManager.createQuery(criteriaEntity)
+                    .getResultStream()
+                    .findFirst()
+                    .orElse(null);
+            result = getDtoMapper().map(entity);
+
+            session.getTransaction().commit();
+        }
+        return result;
+    }
+
+    @Override
+    public DtoJWTUserToken readByRefreshToken(String caption) {
+        DtoJWTUserToken result = null;
+        try(Session session = createConnection()){
+            session.beginTransaction();
+            EntityManagerFactory entityManagerFactory = session.getEntityManagerFactory();
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<EntityJWTUserToken> criteriaEntity = criteriaBuilder.createQuery(getEntityClass());
+            Root<EntityJWTUserToken> criteriaRoot = criteriaEntity.from(getEntityClass());
+            criteriaEntity.select(criteriaRoot);
+            criteriaEntity.where(criteriaBuilder.equal(criteriaRoot.get("refreshToken"), caption));
+            EntityJWTUserToken entity = entityManager.createQuery(criteriaEntity)
+                    .getResultStream()
+                    .findFirst()
+                    .orElse(null);
+            result = getDtoMapper().map(entity);
+
+            session.getTransaction().commit();
+        }
+        return result;
     }
 }
--- jdb/jdb-hibernate/src/main/java/ru/strict/db/hibernate/repositories/RepositoryUser.java
+++ jdb/jdb-hibernate/src/main/java/ru/strict/db/hibernate/repositories/RepositoryUser.java
@@ -1,14 +1,26 @@
 package ru.strict.db.hibernate.repositories;
 
+import org.hibernate.Session;
 import ru.strict.db.core.common.GenerateIdType;
 import ru.strict.db.core.dto.DtoUserBase;
 import ru.strict.db.core.mappers.dto.MapperDtoBase;
+import ru.strict.db.core.repositories.interfaces.IRepositoryUser;
 import ru.strict.db.hibernate.connection.CreateConnectionHibernate;
 import ru.strict.db.hibernate.entities.EntityUser;
 
-public class RepositoryUser<DTO extends DtoUserBase> extends RepositoryNamedBase<EntityUser, DTO> {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.UUID;
 
-    private static final String[] COLUMNS_NAME = new String[] {"username", "passwordencode"};
+public class RepositoryUser<DTO extends DtoUserBase>
+        extends RepositoryNamedBase<EntityUser, DTO>
+        implements IRepositoryUser<UUID, DTO> {
+
+    private static final String[] COLUMNS_NAME = new String[] {"username", "passwordencode", "email",
+            "isBlocked", "isDeleted", "isConfirmEmail"};
 
     public RepositoryUser(CreateConnectionHibernate connectionSource,
                           MapperDtoBase<EntityUser, DTO> dtoMapper,
@@ -28,6 +40,29 @@ public class RepositoryUser<DTO extends DtoUserBase> extends RepositoryNamedBase
     @Override
     protected String getColumnWithName() {
         return COLUMNS_NAME[0];
+    }
+
+    @Override
+    public DTO readByEmail(String email) {
+        DTO result = null;
+        try(Session session = createConnection()){
+            session.beginTransaction();
+            EntityManagerFactory entityManagerFactory = session.getEntityManagerFactory();
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<EntityUser> criteriaEntity = criteriaBuilder.createQuery(getEntityClass());
+            Root<EntityUser> criteriaRoot = criteriaEntity.from(getEntityClass());
+            criteriaEntity.select(criteriaRoot);
+            criteriaEntity.where(criteriaBuilder.equal(criteriaRoot.get("email"), email));
+            EntityUser entity = entityManager.createQuery(criteriaEntity)
+                    .getResultStream()
+                    .findFirst()
+                    .orElse(null);
+            result = getDtoMapper().map(entity);
+
+            session.getTransaction().commit();
+        }
+        return result;
     }
 
     @Override
