42558cf030cad93c2c0bb67bae2be0f9d6331d1b
Konstantin <strictkod1899@yandex.ru>
2019-01-06 21:02:47 +0700

    feature (jutils): implemented util to proces html

---
+++ jutils/src/main/java/ru/strict/utils/UtilHtml.java
@@ -0,0 +1,82 @@
+package ru.strict.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import ru.strict.validates.ValidateBaseValue;
+
+import java.io.File;
+import java.io.IOException;
+
+public class UtilHtml {
+
+    public static Elements selectByFileUTF8(String filePath, String selector) throws IOException, NullPointerException {
+        return selectByFile(filePath, selector, "UTF-8");
+    }
+
+    public static Elements selectByFile(String filePath, String selector, String encoding) throws IOException, NullPointerException {
+        if(ValidateBaseValue.isEmptyOrNull(filePath)){
+            throw new NullPointerException("filePath is NULL");
+        }
+
+        Document page = Jsoup.parse(new File(filePath), encoding);
+        if(page == null){
+            throw new NullPointerException(String.format("Page by content [%s] not found", filePath));
+        }
+
+        return getTag(page, selector);
+    }
+
+    public static Elements selectByContent(String content, String selector) throws IOException, NullPointerException {
+        if(ValidateBaseValue.isEmptyOrNull(content)){
+            throw new NullPointerException("content is NULL");
+        }
+
+        Document page = Jsoup.parseBodyFragment(content);
+        if(page == null){
+            throw new NullPointerException(String.format("Page by content [%s] not found", content));
+        }
+
+        return getTag(page, selector);
+    }
+
+    public static Elements selectByGet(String url, String selector) throws IOException, NullPointerException {
+        if(ValidateBaseValue.isEmptyOrNull(url)){
+            throw new NullPointerException("url is NULL");
+        }
+
+        Document page = Jsoup.connect(url).get();
+        if(page == null){
+            throw new NullPointerException(String.format("Page by url [%s] [GET] not found", url));
+        }
+
+        return getTag(page, selector);
+    }
+
+    public static Elements selectByPost(String url, String selector) throws IOException, NullPointerException {
+        if(ValidateBaseValue.isEmptyOrNull(url)){
+            throw new NullPointerException("url is NULL");
+        }
+
+        Document page = Jsoup.connect(url).post();
+        if(page == null){
+            throw new NullPointerException(String.format("Page by url [%s] [POST] not found", url));
+        }
+
+        return getTag(page, selector);
+    }
+
+    private static Elements getTag(Document page, String selector){
+        Elements tags = null;
+        if(ValidateBaseValue.isEmptyOrNull(selector)){
+            tags = page.getAllElements();
+        } else {
+            tags = page.select(selector);
+        }
+        if(tags.size() == 0){
+            tags = null;
+        }
+
+        return tags;
+    }
+}
