11bcfe834abcf1221c22b4479a0207cb79f9aa3c
Konstantin <strictkod1899@yandex.ru>
2019-02-22 09:55:50 +0700

    feature (jpatterns): added interface Builder

---
+++ jpatterns/src/main/java/ru/strict/patterns/IBuilder.java
@@ -0,0 +1,13 @@
+package ru.strict.patterns;
+
+/**
+ * Pattern 'Builder'. Описание базовой функцональности строителя
+ * @param <RESULT> Тип создаваемого экземпляра
+ */
+public interface IBuilder<RESULT> {
+
+	/**
+	* Создать экземпляр класса
+	*/
+	RESULT build();
+}
