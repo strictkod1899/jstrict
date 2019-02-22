67a1afaac4c5fff3505bcb6ea7b432234b65114b
Konstantin <strictkod1899@yandex.ru>
2019-02-22 09:31:58 +0700

    feature (jutils): formetted output-json

--- jutils/src/main/java/ru/strict/utils/UtilJson.java
+++ jutils/src/main/java/ru/strict/utils/UtilJson.java
@@ -3,13 +3,21 @@ package ru.strict.utils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class UtilJson {
 
+    /**
+     * Пример использования:
+     * UtilJson.saveToJson(this, pathToFile);
+     */
     public static <O extends Object> void saveToJson(O object, String pathToJson){
         try {
             ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
             mapper.writeValue(new File(pathToJson), object);
         }catch(IOException ex){
             throw new RuntimeException(ex);
