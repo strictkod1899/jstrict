5e2a00e3b49003d374c4fef4d841729c151a4d62
Konstantin <strictkod1899@yandex.ru>
2019-06-14 09:58:56 +0700

    fixed (jutils): fixed save to json

--- jutils/src/main/java/ru/strict/utils/UtilJson.java
+++ jutils/src/main/java/ru/strict/utils/UtilJson.java
@@ -3,10 +3,13 @@ package ru.strict.utils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import ru.strict.validates.ValidateBaseValue;
 
 public class UtilJson {
 
@@ -15,16 +18,32 @@ public class UtilJson {
      * UtilJson.saveToJson(this, pathToFile);
      */
     public static <O extends Object> void saveToJson(O object, String pathToJson){
+        if(object == null){
+            throw new IllegalArgumentException("object for write into json is NULL");
+        }
+        if(ValidateBaseValue.isEmptyOrNull(pathToJson)){
+            throw new IllegalArgumentException("pathToJson is NULL");
+        }
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            mapper.writeValue(new File(pathToJson), object);
+            if(object instanceof String){
+                UtilFile.saveFile(pathToJson, object.toString());
+            } else {
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                mapper.writeValue(new File(pathToJson), object);
+            }
         }catch(IOException ex){
             throw new RuntimeException(ex);
         }
     }
 
     public static <O> O loadFromJson(String pathToJson, Class<O> clazz){
+        if(clazz == null){
+            throw new IllegalArgumentException("class for read from json is NULL");
+        }
+        if(ValidateBaseValue.isEmptyOrNull(pathToJson)){
+            throw new IllegalArgumentException("pathToJson is NULL");
+        }
         try {
             ObjectMapper mapper = new ObjectMapper();
             return mapper.readValue(new File(pathToJson), clazz);
@@ -34,6 +53,12 @@ public class UtilJson {
     }
 
     public static <O> O loadFromJson(InputStream stream, Class<O> clazz){
+        if(clazz == null){
+            throw new IllegalArgumentException("class for read from json is NULL");
+        }
+        if(stream == null){
+            throw new IllegalArgumentException("stream for read from json is NULL");
+        }
         try {
             ObjectMapper mapper = new ObjectMapper();
             return mapper.readValue(stream, clazz);
