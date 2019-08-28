7f27791bb258b3fb3501c96cf7c5904ed8789e4d
Konstantin <strictkod1899@yandex.ru>
2019-08-28 21:15:12 +0700

    chore (jdb-models): added comments

--- jdb/jdb-models/src/main/java/ru/strict/models/IModelBase.java
+++ jdb/jdb-models/src/main/java/ru/strict/models/IModelBase.java
@@ -2,6 +2,22 @@ package ru.strict.models;
 
 import ru.strict.patterns.MapTarget;
 
+/**
+ * Стандартная реализация поиска enum по id:
+ * <code><pre style="background-color: white; font-family: consolas">
+ * public static MyEnum getById(Integer id){
+ *     if(id == null){
+ *         return null;
+ *     }
+ *
+ *     return Arrays.stream(MyEnum.values())
+ *         .filter(item -> item.id == id)
+ *         .findFirst()
+ *         .orElse(null);
+ * }
+ * </pre></code>
+ * @return
+ */
 public interface IModelBase<ID> extends MapTarget {
     ID getId();
 }
