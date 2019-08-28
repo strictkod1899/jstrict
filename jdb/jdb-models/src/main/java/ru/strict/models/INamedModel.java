717582af5583ce416f6ed502f4954d0863187c87
Konstantin <strictkod1899@yandex.ru>
2019-08-28 21:15:45 +0700

    chore (jdb-models): added comments

--- jdb/jdb-models/src/main/java/ru/strict/models/INamedModel.java
+++ jdb/jdb-models/src/main/java/ru/strict/models/INamedModel.java
@@ -1,5 +1,21 @@
 package ru.strict.models;
 
+/**
+ * Стандартная реализация поиска enum по caption:
+ * <code><pre style="background-color: white; font-family: consolas">
+ * public static MyEnum getByCaption(String caption){
+ *     if(caption == null){
+ *         return null;
+ *     }
+ *
+ *     return Arrays.stream(MyEnum.values())
+ *         .filter(item -> item.caption.equals(caption))
+ *         .findFirst()
+ *         .orElse(null);
+ * }
+ * </pre></code>
+ * @return
+ */
 public interface INamedModel<ID> extends IModelBase<ID> {
     String getCaption();
 }
