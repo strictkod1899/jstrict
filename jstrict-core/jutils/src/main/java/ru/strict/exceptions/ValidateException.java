fd070af512886ef456ea06de686fb19209749ac1
Konstantin <strictkod1899@yandex.ru>
2020-05-14 23:13:24 +0700

    fixed (jutils): исправлены ошибки Validator

--- jstrict-core/jutils/src/main/java/ru/strict/exceptions/ValidateException.java
+++ jstrict-core/jutils/src/main/java/ru/strict/exceptions/ValidateException.java
@@ -1,23 +1,37 @@
 package ru.strict.exceptions;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ValidateException extends RuntimeException {
+    private List<String> valuesNames;
+    private List<String> reasons;
+    private List<Object> values;
+    private String details;
 
     public ValidateException(String valueName) {
         super(String.format("Value is not valid [%s]", valueName));
+        this.valuesNames = Collections.singletonList(valueName);
     }
 
     public ValidateException(String valueName, String reason) {
         super(String.format("Value is not valid [%s]. Reason = [%s]", valueName, reason));
+        this.valuesNames = Collections.singletonList(valueName);
+        this.reasons = Collections.singletonList(reason);
     }
 
     public ValidateException(String valueName, String reason, String details) {
         super(String.format("Value is not valid [%s]. Reason = [%s]. Details: %s", valueName, reason, details));
+        this.valuesNames = Collections.singletonList(valueName);
+        this.reasons = Collections.singletonList(reason);
+        this.details = details;
     }
 
     public ValidateException(String valueName, String reason, Object value) {
         super(String.format("Value is not valid [%s]. Reason = [%s]. Value = [%s]", valueName, reason, value));
+        this.valuesNames = Collections.singletonList(valueName);
+        this.reasons = Collections.singletonList(reason);
+        this.values = Collections.singletonList(value);
     }
 
     public ValidateException(String valueName, String reason, Object value, String details) {
@@ -26,26 +40,43 @@ public class ValidateException extends RuntimeException {
                 reason,
                 value,
                 details));
+        this.valuesNames = Collections.singletonList(valueName);
+        this.reasons = Collections.singletonList(reason);
+        this.values = Collections.singletonList(value);
+        this.details = details;
     }
 
     public ValidateException(List<String> valuesNames) {
         super(String.format("Values is not valid [%s]", valuesNames));
+        this.valuesNames = valuesNames;
     }
 
     public ValidateException(List<String> valuesNames, List<String> reasons) {
         super(formatMessage(valuesNames, reasons, null, null));
+        this.valuesNames = valuesNames;
+        this.reasons = reasons;
     }
 
     public ValidateException(List<String> valuesNames, List<String> reasons, String details) {
         super(formatMessage(valuesNames, reasons, null, details));
+        this.valuesNames = valuesNames;
+        this.reasons = reasons;
+        this.details = details;
     }
 
     public ValidateException(List<String> valuesNames, List<String> reasons, List<Object> values) {
         super(formatMessage(valuesNames, reasons, values, null));
+        this.valuesNames = valuesNames;
+        this.reasons = reasons;
+        this.values = values;
     }
 
     public ValidateException(List<String> valuesNames, List<String> reasons, List<Object> values, String details) {
         super(formatMessage(valuesNames, reasons, values, details));
+        this.valuesNames = valuesNames;
+        this.reasons = reasons;
+        this.values = values;
+        this.details = details;
     }
 
     private static String formatMessage(List<String> valuesNames,
@@ -74,5 +105,21 @@ public class ValidateException extends RuntimeException {
         } else {
             return String.format("Values is not valid. %s. Reasons [%s]", details, errorText.toString());
         }
+    }
+
+    public List<String> getValuesNames() {
+        return valuesNames;
+    }
+
+    public List<String> getReasons() {
+        return reasons;
+    }
+
+    public List<Object> getValues() {
+        return values;
+    }
+
+    public String getDetails() {
+        return details;
     }
 }
