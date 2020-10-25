ca6eadc24cd1fd823124dfbec58f366ff52afbfc
Konstantin <strictkod1899@yandex.ru>
2020-10-25 07:45:06 +0700

    feature (jutils): реализованы build-методы для AlertException

--- jstrict-core/jutils/src/main/java/ru/strict/components/IExceptionProvider.java
+++
@@ -1,7 +0,0 @@
-package ru.strict.components;
-
-public interface IExceptionProvider {
-    void onThrow(IMessageCode messageCode, Object[] args);
-
-    void onThrow(String messageCode, Object[] args);
-}
--- jstrict-core/jutils/src/main/java/ru/strict/components/IMessageProvider.java
+++ jstrict-core/jutils/src/main/java/ru/strict/components/IMessageProvider.java
@@ -4,6 +4,5 @@ public interface IMessageProvider {
     default String getMessage(IMessageCode messageCode) {
         return getMessage(messageCode.getCode());
     }
-
     String getMessage(String messageCode);
 }
