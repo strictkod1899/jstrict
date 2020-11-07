e24ee44ee2464370dc9cdf52987bc8f29fc75bd5
Konstantin <strictkod1899@yandex.ru>
2020-11-07 11:08:45 +0300

    feature (deploy): реализованы скрипты для установки всех компонентов jstrict сразу

---
+++ deploy/install.ps1
@@ -0,0 +1,5 @@
+mvn clean install -f ../jstrict-parent/pom.xml
+mvn clean install -f ../jstrict-core/pom.xml
+mvn clean install -f ../jneuralnetwork/pom.xml
+mvn clean install -f ../ioc/pom.xml
+mvn clean install -f ../jdb/pom.xml
---
+++ deploy/install_skip_tests.ps1
@@ -0,0 +1,5 @@
+mvn clean install -DskipTests -f ../jstrict-parent/pom.xml
+mvn clean install -DskipTests -f ../jstrict-core/pom.xml
+mvn clean install -DskipTests -f ../jneuralnetwork/pom.xml
+mvn clean install -DskipTests -f ../ioc/pom.xml
+mvn clean install -DskipTests -f ../jdb/pom.xml
