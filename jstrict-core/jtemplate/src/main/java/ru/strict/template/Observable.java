02a104f20a709bd438ecc18e38d26129672435a6
Konstantin <strictkod1899@yandex.ru>
2021-06-24 14:38:28 +0700

    feature (jtemplate): интерфейсы паттерна Observer сделал просто маркерными

--- jstrict-core/jtemplate/src/main/java/ru/strict/template/Observable.java
+++ jstrict-core/jtemplate/src/main/java/ru/strict/template/Observable.java
@@ -3,17 +3,14 @@ package ru.strict.template;
 /**
  * Pattern 'Observer'. Описание базовой функциональности наблюдаемого класса (класс за которым наблюдает Observer)
  *
+ * <pre>
+ *     Добавить класс-наблюдатель за текущим объектом
+ *     void addObserver(T observer);
+ *
+ *     Удалить класс-наблюдатель
+ *     void removeObserver(T observer);
+ * </pre>
  * @param <T> Типы объектов, которые могут наблюдать за данным экземпляром
  */
-public interface IObservable<T> {
-
-    /**
-     * Добавить класс-наблюдатель за текущим объектом
-     */
-    void addObserver(T observer);
-
-    /**
-     * Удалить класс-наблюдатель
-     */
-    void removeObserver(T observer);
+public interface Observable {
 }
