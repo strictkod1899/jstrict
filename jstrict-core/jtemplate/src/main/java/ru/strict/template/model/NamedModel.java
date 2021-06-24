102af4b68f76bb88d8072b5d1eb8b1b15d3b1358
Konstantin <strictkod1899@yandex.ru>
2021-06-24 22:53:34 +0700

    feature (jtemplate): добавил equals и hashcode в базовые классы модели

--- jstrict-core/jtemplate/src/main/java/ru/strict/template/model/NamedModel.java
+++ jstrict-core/jtemplate/src/main/java/ru/strict/template/model/NamedModel.java
@@ -1,12 +1,18 @@
 package ru.strict.template.model;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class NamedModel<ID> extends BaseModel<ID> implements INamedModel<ID> {
     private String name;
 
-    //<editor-fold defaultState="collapsed" desc="constructors">
-    public NamedModel() {}
+    public NamedModel() {
+        super();
+    }
 
     public NamedModel(String name) {
         super();
@@ -17,42 +23,4 @@ public class NamedModel<ID> extends BaseModel<ID> implements INamedModel<ID> {
         super(id);
         this.name = name;
     }
-    //</editor-fold>
-
-    //<editor-fold defaultState="collapsed" desc="Get/Set">
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    //</editor-fold>
-
-    //<editor-fold defaultState="collapsed" desc="Base override">
-    @Override
-    public String toString() {
-        return "NamedModel{" +
-                "name='" + name + '\'' +
-                "} " + super.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        NamedModel<ID> model = (NamedModel<ID>) o;
-        return Objects.equals(name, model.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-    //</editor-fold>
 }
