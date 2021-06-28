315480a67b8494c148789cd3df6674a0004525c8
Konstantin <strictkod1899@yandex.ru>
2021-06-28 12:05:04 +0700

    feature (deploy): объединил проекты в общий pom-parent

--- deploy/deploy_new_version.ps1
+++ deploy/deploy_new_version.ps1
@@ -1,31 +1,5 @@
-cd ./jstrict-parent
-mvn clean install
+# Полный deploy проекта с обновлением версии в режиме prod
+# Скрипт запускать из корневой директории проекта
 
-cd ../jstrict-core
-./deploy/deploy_new_version.ps1
-
-cd ../jneuralnetwork
-./deploy/deploy_new_version.ps1
-
-cd ../ioc
-./deploy/deploy_new_version.ps1
-
-cd ..
-
-
-# constants
-$PROD_MODE = "prod"
-$DEV_MODE = "dev"
-$FEATURE_MODE = "feature"
-
-$VERSIONS_FILE = "./jstrict-core/deploy/versions.properties"
-
-# commit new version
 $mode = &"./deploy/other/get_deploy_mode_by_git_branch.ps1" | Select-Object -Last 1
-$buildVersion = &"./deploy/version/get_current_version_by_mode.ps1" -versionsFile "${VERSIONS_FILE}" -prodMode "${PROD_MODE}" -devMode "${DEV_MODE}" -featureMode "${FEATURE_MODE}" -mode "${mode}" | Select-Object -Last 1
-
-./deploy/git/git_commit.ps1 -message "chore (deploy): update version ${buildVersion}"
-	
-if ($mode -eq $PROD_MODE) {
-	./deploy/git/git_create_or_update_tag.ps1 -tag "${buildVersion}"
-}
+./deploy/deploy.ps1 -mode "${mode}"
--- ioc/deploy/git/get_current_branch.ps1
+++
@@ -1,5 +0,0 @@
-# Получить имя текущей ветки git
-# Пример использования скрипта:
-#    | $branchName = &"./get_current_branch.ps1" | Select-Object -Last 1
-
-return git rev-parse --abbrev-ref HEAD
