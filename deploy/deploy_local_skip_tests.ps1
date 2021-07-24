cc62a2d37a5900f04e8b070f7706b544d786bf95
Konstantin <strictkod1899@yandex.ru>
2021-07-24 17:19:35 +0700

    feature (deploy): обновил скрипты деплоя

--- deploy/deploy_local_skip_tests.ps1
+++ deploy/deploy_local_skip_tests.ps1
@@ -1,5 +1,14 @@
 # Сборка проекта без запуска тестов (обновление версии и push в ветку git не выполняется)
 # Скрипт запускать из корневой директории проекта
 
-$mode = &"./deploy/other/get_deploy_mode_by_git_branch.ps1" | Select-Object -Last 1
+param(
+	# optional
+	# Режим: prod, dev, feature
+	[string] $mode
+)
+
+if ($mode -eq $null -Or $mode -eq '') {
+    $mode = &"./deploy/other/get_deploy_mode_by_git_branch.ps1" | Select-Object -Last 1
+}
+
 ./deploy/deploy.ps1 -mode "${mode}" -skipGit $True -skipTests $True -skipUpdateVersion $True
